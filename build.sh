75cafdf2727edc2934db7cc8fc4299aa0a10ec2f
vortex314 <lieven.merckx@gmail.com>
2020-11-18 23:46:55 +0100

    updated makePaho and build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@ echo "run from workspace directory !"
 #  INSTALL OPENSSL dev libraries first
 # sudo apt-get install libssl-dev
 #
+sudo apt-get install libssl-dev
+git clone https://github.com/bblanchon/ArduinoJson.git
 git clone https://github.com/eclipse/paho.mqtt.c.git
-git clone https://github.com/pantor/inja.git
-git clone https://github.com/nlohmann/json.git
 git clone https://github.com/vortex314/Common
-git clone https://github.com/vortex314/serial2mqtt
+git clone https://github.com/vortex314/joystick2mqtt
 cd Common
 make -f Common.mk
 cd ../paho.mqtt.c
 make
 cp ../serial2mqtt/makePaho.sh .
 ./makePaho.sh 
-cd ../serial2mqtt
+cd ../joystick2mqtt
 #
 # change all paths in the .mk file to your paths
 #
-make -f serial2mqtt.mk
+make -f joystick2mqtt.mk

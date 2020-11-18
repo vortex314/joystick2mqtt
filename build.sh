4026086b00f73209afc1aeebc363f54179e62df8
vortex314 <lieven.merckx@gmail.com>
2020-11-18 23:38:22 +0100

    added build scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+echo "run from workspace directory !"
+#  INSTALL OPENSSL dev libraries first
+# sudo apt-get install libssl-dev
+#
+git clone https://github.com/eclipse/paho.mqtt.c.git
+git clone https://github.com/pantor/inja.git
+git clone https://github.com/nlohmann/json.git
+git clone https://github.com/vortex314/Common
+git clone https://github.com/vortex314/serial2mqtt
+cd Common
+make -f Common.mk
+cd ../paho.mqtt.c
+make
+cp ../serial2mqtt/makePaho.sh .
+./makePaho.sh 
+cd ../serial2mqtt
+#
+# change all paths in the .mk file to your paths
+#
+make -f serial2mqtt.mk
